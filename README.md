d7e3260c8d1cb51a6f7115430e4abefeb2b92510
Fabian Grutschus <f.grutschus@lubyte.de>
2017-08-15 14:07:42 +0200

    Added package and info to README

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# sid
-Converter for Security Identifiers (SID)
+# SID converter for Go
+
+Converter for Security Identifiers (SID) from binary to string.
+
+## Installation
+
+```
+go get github.com/fabiang/sid
+```
+
+## Usage
+
+```go
+import (
+  "fmt"
+  "log"
+
+  "github.com/fabiang/sid"
+)
+
+func main() {
+  mysid := []byte{
+    1,
+    5,
+    0,
+    0,
+    0,
+    0,
+    0,
+    5,
+    21,
+    0,
+    0,
+    0,
+    196,
+    235,
+    38,
+    74,
+    26,
+    193,
+    247,
+    92,
+    104,
+    142,
+    125,
+    166,
+    107,
+    6,
+    0,
+    0}
+
+  converted, err := sid.ConvertToString(mysid)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  fmt.Printf("SID: %s\n", converted)
+}
+```
+
+## License
+
+[BSD 2-Clause License](LICENSE)
---
+++ sid.go
@@ -0,0 +1,45 @@
+package sid
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+)
+
+const (
+	MASK_8_BIT  = 0xff
+	MASK_32_BIT = 0xffffffff
+	MASK_48_BIT = 0xffffffffffff
+)
+
+func ConvertToString(input []byte) (string, error) {
+	length := len(input)
+
+	if len(input) < 8 {
+		return "", fmt.Errorf("Byte string given must have at least 8 bytes, but got only %d bytes", length)
+	}
+
+	revision := input[0] & MASK_8_BIT
+	numberOfSubAuthorityParts := int(input[1] & MASK_8_BIT)
+
+	check := 8 + numberOfSubAuthorityParts*4
+	if length != check {
+		return "", fmt.Errorf("According to byte 1 of the SID it total length should be %d bytes, however its actual length is %d bytes", check, length)
+	}
+
+	buf := bytes.NewReader(input)
+
+	var authority uint64
+	binary.Read(buf, binary.BigEndian, &authority)
+
+	var subAuthority = make([]uint32, numberOfSubAuthorityParts)
+	for i := 0; i < numberOfSubAuthorityParts; i++ {
+		binary.Read(buf, binary.LittleEndian, &subAuthority[i])
+	}
+
+	stringValue := fmt.Sprintf("S-%d-%d", revision, authority&MASK_48_BIT)
+	for j := 0; j < numberOfSubAuthorityParts; j++ {
+		stringValue += fmt.Sprintf("-%d", subAuthority[j]&MASK_32_BIT)
+	}
+	return stringValue, nil
+}
